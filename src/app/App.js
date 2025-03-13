4ebd951380e28b5335da3e2e1a63b827721d515d
Gfhjkmfl@1@1 <kostechkina@gmail.com>
2025-03-13 21:21:04 +0400

    change

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,12 @@ import { AppRouter } from "./router";
 import "./styles/index.css";
 
 function App() {
- 
+  document.addEventListener("scroll", function () {
+    if (window.scrollY < 0) {
+      window.scrollTo(0, 0);
+    }
+  });
+
   return (
     <div className="app dark">
       <AppRouter />
